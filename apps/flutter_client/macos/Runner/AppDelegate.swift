09322a312be4d5ac37f527d088e6b52ae9bbd091
yhan-sun <304309934+yhan-sun@users.noreply.github.com>
2026-07-30 12:19:31 +0800

    fix(flutter): foreground macos window on launch

--- apps/flutter_client/macos/Runner/AppDelegate.swift
+++ apps/flutter_client/macos/Runner/AppDelegate.swift
@@ -7,6 +7,17 @@ class AppDelegate: FlutterAppDelegate {
     return true
   }
 
+  override func applicationShouldHandleReopen(
+    _ sender: NSApplication,
+    hasVisibleWindows flag: Bool
+  ) -> Bool {
+    for window in sender.windows {
+      window.makeKeyAndOrderFront(self)
+    }
+    sender.activate(ignoringOtherApps: true)
+    return true
+  }
+
   override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
     return true
   }
