de854685c2c27941b0f1ae539d2626cfac95bcf1
yuhan <syh23120@gmail.com>
2026-09-25 12:39:02 +0800

    feat:切换后台统一样式入口

--- server/admin-ui/src/main.tsx
+++ server/admin-ui/src/main.tsx
@@ -2,11 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import '@xyflow/react/dist/style.css'
-import './styles.css'
-import './polish.css'
-import './connections.css'
-import './connection-health.css'
-import './modern.css'
+import './styles/app.css'
 import App from './App'
 
 const queryClient = new QueryClient({
