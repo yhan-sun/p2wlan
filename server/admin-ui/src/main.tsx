f91f3a996eaf54d14dbc3e7fb390605481b13777
yuhan <syh23120@gmail.com>
2026-09-25 12:41:16 +0800

    feat:启动时应用后台主题

--- server/admin-ui/src/main.tsx
+++ server/admin-ui/src/main.tsx
@@ -4,6 +4,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import '@xyflow/react/dist/style.css'
 import './styles/app.css'
 import App from './App'
+import { applyTheme, readThemeMode } from './shared/theme'
+
+applyTheme(readThemeMode())
 
 const queryClient = new QueryClient({
   defaultOptions: {
